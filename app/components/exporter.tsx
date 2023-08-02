76a5241cbf20aa622a00befe34f3b64805f36211
Tivonfeng <tivonfeng@163.com>
2023-08-02 22:52:47 +0800

    update0802

--- app/components/exporter.tsx
+++ app/components/exporter.tsx
@@ -470,9 +470,9 @@ export function ImagePreviewer(props: {
           </div>
 
           <div>
-            <div className={styles["main-title"]}>ChatGPT Next Web</div>
+            <div className={styles["main-title"]}>绿旗ChatGPT</div>
             <div className={styles["sub-title"]}>
-              github.com/Yidadaa/ChatGPT-Next-Web
+              人工智能启蒙教育践行者
             </div>
             <div className={styles["icons"]}>
               <ExportAvatar avatar={config.avatar} />
